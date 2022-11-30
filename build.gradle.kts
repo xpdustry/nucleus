d9f6d8bdaf97b1658603100a8f042b93bf5d0113
phinner <62483793+Phinner@users.noreply.github.com>
2022-11-30 22:59:15 +0100

    CalVer versioning system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
+import java.time.Clock
+import java.time.LocalDateTime
+
 plugins {
     id("nucleus.parent-conventions")
 }
 
 group = "fr.xpdustry"
 description = "The software collection powering Xpdustry."
-version = "1.0.0" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
+version = file("VERSION.txt").readLines().first()
+
+tasks.register("incrementVersionFile") {
+    doLast {
+        val today = LocalDateTime.now(Clock.systemUTC())
+        val previous = version.toString().split('.').map(String::toInt)
+        val build = if (today.year == previous[0] && today.monthValue == previous[1]) previous[2] + 1 else 0
+        file("VERSION.txt").writeText("${today.year}.${today.monthValue}.$build")
+    }
+}
