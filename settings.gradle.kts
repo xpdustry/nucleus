4926133bd2945b10830a10e3e541e569360f9914
phinner <62483793+Phinner@users.noreply.github.com>
2023-04-16 20:50:49 +0200

    chore: Moved nucleus-build-logic to buildSrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,9 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-pluginManagement {
-    @Suppress("UnstableApiUsage")
-    includeBuild("nucleus-build-logic")
-}
-
 rootProject.name = "nucleus-parent"
 
 include("nucleus-api")
 include("nucleus-common")
-include("nucleus-core")
 include("nucleus-discord")
 include("nucleus-mindustry")
 include("nucleus-mindustry-testing")
